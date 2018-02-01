629e47e5bc09a1bbc52f6861981fad2a41f50aed
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-02 00:49:03 +0530

    MainActivity renamed to welcome activity and LoginActivity created

---
+++ app/src/test/java/com/bugfreetechnology/androidrobolectricdemo/SandwichTest.java
@@ -0,0 +1,9 @@
+package com.bugfreetechnology.androidrobolectricdemo;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class SandwichTest {
+
+}
